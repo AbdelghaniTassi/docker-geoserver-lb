67a5db3e1652f0e3d3e53c68e29a1822bd8a0657
Andreas Petersson <apesv03@gmail.com>
2017-10-14 16:19:09 +0200

    Bump geoserver 2.12-RC1 to 2.12.0

--- images/2.12/build.sh
+++ images/2.12/build.sh
@@ -18,9 +18,8 @@ then
     mkdir $DIR/resources/fonts
 fi
 
-GS_VERSION=2.12-RC1
-GS_COMMUNITY_TAG=master
-GS_COMMUNITY_VERSION=2.12-SNAPSHOT
+GS_VERSION=2.12.0
+GS_COMMUNITY_VERSION=2.12
 
 # Add in selected plugins.  Comment out or modify as required
 if [ ! -f $DIR/resources/plugins/geoserver-control-flow-plugin.zip ]
@@ -47,13 +46,39 @@ if [ ! -f $DIR/resources/plugins/geoserver-libjpeg-turbo-plugin.zip ]
 then
     wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-libjpeg-turbo-plugin.zip -O $DIR/resources/plugins/geoserver-libjpeg-turbo-plugin.zip
 fi
-#if [ ! -f $DIR/resources/plugins/geoserver-mbstyle-plugin.zip ]
-#then
-#    wget -c http://ares.opengeo.org/geoserver/${GS_COMMUNITY_TAG}/community-latest/geoserver-${GS_COMMUNITY_VERSION}-mbstyle-plugin.zip -O $DIR/resources/plugins/geoserver-mbstyle-plugin.zip
-#fi
-#if [ ! -f $DIR/resources/plugins/geoserver-mbtiles-plugin.zip ]
-#then
-#    wget -c http://ares.opengeo.org/geoserver/${GS_COMMUNITY_TAG}/community-latest/geoserver-${GS_COMMUNITY_VERSION}-mbtiles-plugin.zip -O $DIR/resources/plugins/geoserver-mbtiles-plugin.zip
-#fi
+if [ ! -f $DIR/resources/plugins/geoserver-ogr-wfs-plugin.zip ]
+then
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-ogr-wfs-plugin.zip -O $DIR/resources/plugins/geoserver-ogr-wfs-plugin.zip
+fi
+if [ ! -f $DIR/resources/plugins/geoserver-ogr-wps-plugin.zip ]
+then
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-ogr-wps-plugin.zip -O $DIR/resources/plugins/geoserver-ogr-wps-plugin.zip
+fi
+if [ ! -f $DIR/resources/plugins/geoserver-wps-plugin.zip ]
+then
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-wps-plugin.zip -O $DIR/resources/plugins/geoserver-wps-plugin.zip
+fi
+if [ ! -f $DIR/resources/plugins/geoserver-css-plugin.zip ]
+then
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-css-plugin.zip -O $DIR/resources/plugins/geoserver-css-plugin.zip
+fi
+if [ ! -f $DIR/resources/plugins/geoserver-pyramid-plugin.zip ]
+then
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-pyramid-plugin.zip -O $DIR/resources/plugins/geoserver-pyramid-plugin.zip
+fi
+if [ ! -f $DIR/resources/plugins/geoserver-importer-plugin.zip ]
+then
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-importer-plugin.zip -O $DIR/resources/plugins/geoserver-importer-plugin.zip
+fi
+
+# Community plugins
+if [ ! -f $DIR/resources/plugins/geoserver-mbstyle-plugin.zip ]
+then
+    wget -c http://ares.boundlessgeo.com/geoserver/${GS_COMMUNITY_VERSION}.x/community-latest/geoserver-${GS_COMMUNITY_VERSION}-SNAPSHOT-mbstyle-plugin.zip -O $DIR/resources/plugins/geoserver-mbstyle-plugin.zip
+fi
+if [ ! -f $DIR/resources/plugins/geoserver-mbtiles-plugin.zip ]
+then
+    wget -c http://ares.boundlessgeo.com/geoserver/${GS_COMMUNITY_VERSION}.x/community-latest/geoserver-${GS_COMMUNITY_VERSION}-SNAPSHOT-mbstyle-plugin.zip -O $DIR/resources/plugins/geoserver-mbtiles-plugin.zip
+fi
 
 docker build --build-arg TOMCAT_EXTRAS=false -t wootapa/geoserver:2.12 $DIR
